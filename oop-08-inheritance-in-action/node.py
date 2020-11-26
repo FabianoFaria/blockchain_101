b76ce5517638d778f5b464a0b1a405c4b9a20546
Fabiano <fabiano_hatori@yahoo.com.br>
2020-11-26 01:53:47 -0300

    Adicao de UI de gerenciamento de Nodes Adicao de gerenciamento de multiplos servers para Nodes

--- oop-08-inheritance-in-action/node.py
+++ oop-08-inheritance-in-action/node.py
@@ -5,20 +5,22 @@ from wallet import Wallet
 from blockchain import Blockchain
 
 app = Flask(__name__)
-wallet = Wallet()
-blockchain = Blockchain(wallet.public_key)
 CORS(app)
 
 @app.route('/', methods=['GET'])
-def get_ui():
+def get_node_ui():
     return send_from_directory('ui', 'node.html')
+
+@app.route('/network', methods=['GET'])
+def get_network_ui():
+    return send_from_directory('ui', 'network.html')
 
 @app.route('/wallet', methods=['POST'])
 def create_keys():
     wallet.create_keys()
     if wallet.save_keys():
         global blockchain
-        blockchain = Blockchain(wallet.public_key)
+        blockchain = Blockchain(wallet.public_key, port)
         response = {
             'public_key':wallet.public_key,
             'private_key':wallet.private_key,
@@ -35,7 +37,7 @@ def create_keys():
 def load_keys():
     if wallet.load_keys():
         global blockchain
-        blockchain = Blockchain(wallet.public_key)
+        blockchain = Blockchain(wallet.public_key, port)
         response = {
             'public_key':wallet.public_key,
             'private_key':wallet.private_key,
@@ -140,7 +142,56 @@ def get_chain():
         dict_block['transactions'] = [tx.__dict__ for tx in dict_block['transactions']]
     return jsonify(dict_chain), 200
 
+@app.route('/node', methods=['POST'])
+def add_node():
+    values = request.get_json()
+    if not values :
+        response = {
+            'message':'Nenhum dado atrelado!'
+        }
+        return jsonify(response), 400
+    if 'node' not in values:
+        response = {
+            'message':'Nenhum node atrelado!'
+        }
+        return jsonify(response), 400
+    node = values['node']
+    blockchain.add_peer_node(node)
+    response = {
+        'message': 'Node adicionado com susseco!',
+        'all_nodes': blockchain.get_peer_nodes()
+    }
+    return jsonify(response), 201
+
+@app.route('/node/<node_url>', methods=['DELETE'])
+def remove_node(node_url):
+    if node_url == '' or node_url == None:
+        response = {
+            'message': 'Nenhum node encontrado!'
+        }
+        return jsonify(response), 400
+    blockchain.remove_peer_node(node_url)
+    response = {
+        'message': 'Node removido',
+        'all_nodes': blockchain.get_peer_nodes()
+    }
+    return jsonify(response), 200
+
+@app.route('/nodes', methods=['GET'])
+def get_nodes():
+    nodes = blockchain.get_peer_nodes()
+    response = {
+        'all_nodes': nodes
+    }
+    return jsonify(response), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8081)
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', type=int, default=8082)
+    args = parser.parse_args()
+    port = args.port
+    wallet = Wallet(port)
+    blockchain = Blockchain(wallet.public_key, port)
+    app.run(host='0.0.0.0', port=port)
 
